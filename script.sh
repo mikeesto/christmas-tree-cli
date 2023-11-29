26200ff025e56704077a6935cc8ce17a4f80a30f
Michael Esteban <mickel13@gmail.com>
2023-11-29 21:18:01 +1000

    change wording of christmas to festive

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 # https://paste.ee/r/P4gE8
 
-mkdir christmas_tree
-cd christmas_tree
+mkdir festive_tree
+cd festive_tree
 wget https://raw.githubusercontent.com/mikeesto/christmas-tree-cli/master/tree.py
 wget https://raw.githubusercontent.com/mikeesto/christmas-tree-cli/master/config.json
 sudo apt update -y && sudo apt install python3-pip -y
 pip3 install colored
-echo "~~~ Christmas tree ready ~~~"
+echo "~~~ Festive tree ready ~~~"
